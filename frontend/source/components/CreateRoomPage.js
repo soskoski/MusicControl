17c143e8a811054bc54c0fdd887ec8759d067cea
soskoski <soskoski@gmail.com>
2024-10-19 23:52:01 +0200

    andrej me tepa u fifa

--- frontend/source/components/CreateRoomPage.js
+++ frontend/source/components/CreateRoomPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import {
   Button,
   Grid2,
@@ -14,123 +14,156 @@ import {
   Input,
   responsiveFontSizes,
 } from "@mui/material";
-import { json, Link } from "react-router-dom";
+import { json, Link, Navigate, useNavigate } from "react-router-dom";
 
-export default class CreateRoom extends Component {
-  defaultVotes = 2;
+// export default class CreateRoom extends Component {
+//   defaultVotes = 2;
 
-  constructor(props) {
-    super(props);
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      GuessCanpause: true,
-      VotesToSkip: this.defaultVotes,
-    };
+//     this.state = {
+//       GuessCanpause: true,
+//       VotesToSkip: this.defaultVotes,
+//     };
 
-    this.handleCreateRoomButtonPressed =
-      this.handleCreateRoomButtonPressed.bind(this);
-    this.handleVotesChange = this.handleVotesChange.bind(this);
-    this.handleGuessCanPauseChange = this.handleGuessCanPauseChange.bind(this);
-  }
+//     this.handleCreateRoomButtonPressed =
+//       this.handleCreateRoomButtonPressed.bind(this);
+//     this.handleVotesChange = this.handleVotesChange.bind(this);
+//     this.handleGuessCanPauseChange = this.handleGuessCanPauseChange.bind(this);
+//   }
 
-  handleVotesChange(e) {
-    this.setState({
-      VotesToSkip: e.target.value,
-    });
-  }
-  handleGuessCanPauseChange(e) {
-    this.setState({
-      GuessCanpause: e.target.value === "true" ? true : false,
-    });
-  }
+//   handleVotesChange(e) {
+//     this.setState({
+//       VotesToSkip: e.target.value,
+//     });
+//   }
+//   handleGuessCanPauseChange(e) {
+//     this.setState({
+//       GuessCanpause: e.target.value === "true" ? true : false,
+//     });
+//   }
 
-  handleCreateRoomButtonPressed() {
+//   handleCreateRoomButtonPressed() {
+//     const requestOptions = {
+//       method: "POST",
+//       headers: { "content-Type": "application/json" },
+//       body: JSON.stringify({
+//         votes_to_skip: this.state.VotesToSkip,
+//         guess_can_pause: this.state.GuessCanpause,
+//       }),
+//     };
+
+//     fetch("/api/createRoom", requestOptions)
+//       .then((response) => response.json())
+//       .then((data) => {
+//         const roomCode = data.code;
+//         useNavigate
+//       });
+//   }
+
+const CreateRoom = () => {
+  const defaultVotes = 2;
+  const [GuestCanpause, setGuestCanPause] = useState(false);
+  const [VotesToSkip, setVotesToSkip] = useState(defaultVotes);
+  const navigate = useNavigate();
+
+  const handleGuestCanPauseChange = (e) => {
+    setGuestCanPause(e.target.value == "true");
+  };
+
+  const handleVotesChange = (e) => {
+    setVotesToSkip(e.target.value);
+  };
+
+  const handleCreateRoomButtonPressed = () => {
     const requestOptions = {
       method: "POST",
       headers: { "content-Type": "application/json" },
       body: JSON.stringify({
-        votes_to_skip: this.state.VotesToSkip,
-        guess_can_pause: this.state.GuessCanpause,
+        votes_to_skip: VotesToSkip,
+        guess_can_pause: GuestCanpause,
       }),
     };
 
-    fetch("/api/createRoom", requestOptions)
+    fetch("api/CreateRoom", requestOptions)
       .then((response) => response.json())
-      .then((data) => console.log(data));
-  }
+      .then((data) => {
+        const roomCode = data.code;
+        navigate(`/room/${roomCode}`);
+      });
+  };
 
-  render() {
-    return (
-      <Grid2
-        container
-        spacing={1}
-        direction={"column"}
-        alignItems={"center"}
-        justifyContent={"center"}
-        style={{ height: "100vh" }}
-      >
-        <Grid2 item="true" xs={12} alignItems={"center"}>
-          <Typography component="h4" variant="h4">
-            Create a Room
-          </Typography>
-        </Grid2>
-        <Grid2 alignItems={"center"}>
-          <FormControl>
-            <FormHelperText>Guess Controll of Playback State</FormHelperText>
-            <RadioGroup
-              row
-              defaultValue="true"
-              onChange={this.handleGuessCanPauseChange}
-            >
-              <FormControlLabel
-                value="true"
-                control={<Radio color="primary" />}
-                label="Play/Pause"
-                labelPlacement="bottom"
-              />
-              <FormControlLabel
-                value="false"
-                control={<Radio color="secondary" />}
-                label="No Control"
-                labelPlacement="bottom"
-              />
-            </RadioGroup>
-          </FormControl>
-        </Grid2>
-        <Grid2 item="true" xs={12}>
-          <FormControl>
-            <TextField
-              required={true}
-              type="number"
-              defaultValue={this.defaultVotes}
-              onChange={this.handleVotesChange}
-              slotProps={{
-                htmlInput: {
-                  min: 1,
-                  style: { textAlign: "center" },
-                },
-              }}
-            />
-            <FormHelperText sx={{ textAlign: "center" }}>
-              Votes required to Skip
-            </FormHelperText>
-          </FormControl>
-        </Grid2>
-        <Grid2 item="true" xs={12}>
-          <Button
-            color="primary"
-            variant="contained"
-            onClick={this.handleCreateRoomButtonPressed}
-          >
-            Create a Room
-          </Button>
-        </Grid2>
-        <Grid2 item="true" xs={12}>
-          <Button color="secondary" variant="contained" to="/" component={Link}>
-            Back
-          </Button>
-        </Grid2>
+  return (
+    <Grid2
+      container
+      spacing={1}
+      direction={"column"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      //style={{ height: "100vh" }}
+    >
+      <Grid2 item="true" xs={12} alignItems={"center"}>
+        <Typography component="h4" variant="h4">
+          Create a Room
+        </Typography>
       </Grid2>
-    );
-  }
-}
+      <Grid2 alignItems={"center"}>
+        <FormControl>
+          <FormHelperText>Guess Controll of Playback State</FormHelperText>
+          <RadioGroup
+            row
+            defaultValue="true"
+            onChange={handleGuestCanPauseChange}
+          >
+            <FormControlLabel
+              value="true"
+              control={<Radio color="primary" />}
+              label="Play/Pause"
+              labelPlacement="bottom"
+            />
+            <FormControlLabel
+              value="false"
+              control={<Radio color="secondary" />}
+              label="No Control"
+              labelPlacement="bottom"
+            />
+          </RadioGroup>
+        </FormControl>
+      </Grid2>
+      <Grid2 item="true" xs={12}>
+        <FormControl>
+          <TextField
+            required={true}
+            type="number"
+            defaultValue={defaultVotes}
+            onChange={handleVotesChange}
+            slotProps={{
+              htmlInput: {
+                min: 1,
+                style: { textAlign: "center" },
+              },
+            }}
+          />
+          <FormHelperText sx={{ textAlign: "center" }}>
+            Votes required to Skip
+          </FormHelperText>
+        </FormControl>
+      </Grid2>
+      <Grid2 item="true" xs={12}>
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={handleCreateRoomButtonPressed}
+        >
+          Create a Room
+        </Button>
+      </Grid2>
+      <Grid2 item="true" xs={12}>
+        <Button color="secondary" variant="contained" to="/" component={Link}>
+          Back
+        </Button>
+      </Grid2>
+    </Grid2>
+  );
+};
--- frontend/source/components/Room.js
+++ frontend/source/components/Room.js
@@ -1,29 +1,69 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+// import { useParams } from "react-router-dom";
+
+// export default class Room extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       votesToSkip: 2,
+//       guessCanPause: false,
+//       isHost: false,
+//     };
+//   }
+
+//   render() {
+//     const pars = this.props.params;
+//     console.log(pars);
+//     return (
+//       <div>
+//         {/* <p>Room Code: {roomCode}</p> */}
+//         <p>Votes to Skip: {this.state.votesToSkip}</p>
+//         <p>Guess Can Pause: {this.state.guessCanPause}</p>
+//         <p>Is Host: {this.state.isHost}</p>
+//       </div>
+//     );
+//   }
+// }
+
+// function RoomWrapper(props) {
+//   const { roomCode } = useParams();
+//   return <Room {...props} roomCode={roomCode} />;
+// }
+
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-export default class Room extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      votesToSkip: 2,
-      guessCanPause: false,
-      isHost: false,
-    };
-
-    this.roomCode = this.props.roomCode;
-  }
-
-  render() {
-    return (
-      <div>
-        <h3>{this.roomCode}</h3>
-        <p>Votes to Skip: {this.state.votesToSkip}</p>
-        <p>Guess Can Pause: {this.state.guessCanPause}</p>
-        <p>Is Host: {this.state.isHost}</p>
-      </div>
-    );
-  }
-}
-function fuctionWrapper(props) {
+const Room = () => {
   const { roomCode } = useParams();
-}
+  const [votesToSkip, setVotesToSkip] = useState(2);
+  const [guestCanPause, setGuestCanPause] = useState(true);
+  const [isHost, setIsHost] = useState(false);
+
+  useEffect(() => {
+    GetRoomDetails();
+  }, [roomCode]);
+
+  const GetRoomDetails = () => {
+    fetch(`/api/getRoom?code=${roomCode}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setVotesToSkip(data.votes_to_skip);
+        setGuestCanPause(data.guess_can_pause);
+        setIsHost(data.is_host);
+      })
+      .catch((error) => {
+        console.error("Error fetching Room details:", error);
+      });
+  };
+
+  return (
+    <div>
+      <p>Room Code: {roomCode}</p>
+      <p>Votes to Skip: {votesToSkip}</p>
+      <p>Guess Can Pause: {guestCanPause ? "YES" : "NO".toString()}</p>
+      <p>Is Host: {isHost ? "YES" : "NO".toString()}</p>
+    </div>
+  );
+};
+
+export default Room;
